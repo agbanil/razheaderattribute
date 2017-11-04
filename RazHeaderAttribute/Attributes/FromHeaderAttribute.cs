5997d8cca98433a691863b81a8328f647ca30cec
Lawrence Agbani <lawrence.agbani@konga.local>
2017-11-04 19:34:25 +0000

    Initial commit Added all files

---
+++ RazHeaderAttribute/Attributes/FromHeaderAttribute.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Web.Http;
+using System.Web.Http.Controllers;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RazHeaderAttribute.Bindings;
+
+namespace RazHeaderAttribute.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Parameter, Inherited = true, AllowMultiple = false)]
+    public sealed class FromHeaderAttribute : ParameterBindingAttribute
+    {
+        private readonly string headerName;
+        public FromHeaderAttribute(string headerName)
+        {
+            this.headerName = headerName;
+        }
+
+        public override HttpParameterBinding GetBinding(HttpParameterDescriptor parameter)
+        {
+            return new FromHeaderBinding(parameter, this.headerName);
+        }
+    }
+}
---
+++ RazHeaderAttribute/Bindings/FromHeaderBinding.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http.Controllers;
+using System.Web.Http.Metadata;
+
+namespace RazHeaderAttribute.Bindings
+{
+    public class FromHeaderBinding : HttpParameterBinding
+    {
+        private string name;
+
+        public FromHeaderBinding(HttpParameterDescriptor parameter, string headerName) : base(parameter)
+        {
+            if (string.IsNullOrEmpty(headerName))
+            {
+                throw new ArgumentNullException("headerName");
+            }
+
+            this.name = headerName;
+        }
+
+        public override Task ExecuteBindingAsync(ModelMetadataProvider metadataProvider, HttpActionContext actionContext, CancellationToken cancellationToken)
+        {
+            IEnumerable<string> values;
+            if (actionContext.Request.Headers.TryGetValues(this.name, out values))
+            {
+                actionContext.ActionArguments[this.Descriptor.ParameterName] = Convert.ChangeType(values.FirstOrDefault(), this.Descriptor.ParameterType);
+            }
+            else
+            {
+                actionContext.ActionArguments[this.Descriptor.ParameterName] = Convert.ChangeType(0, this.Descriptor.ParameterType);
+            }
+
+            var taskSource = new TaskCompletionSource<object>();
+            taskSource.SetResult(null);
+            return taskSource.Task;
+        }
+    }
+}
